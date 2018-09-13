e9cfacdaf26ed30fa0b4431d0805a691ff5612a6
trust-freedom <trust_freedom@sina.com>
2018-09-13 17:26:18 +0800

    增加Jest配置，以及通过JestHttpClientConfigBuilderCustomizer自定义HttpClientConfig

---
+++ spring-boot-elasticsearch-jest/src/main/java/com/freedom/springboot/elasticsearch/jest/config/JestHttpClientConfigBuilderCustomizer.java
@@ -0,0 +1,38 @@
+package com.freedom.springboot.elasticsearch.jest.config;
+
+import io.searchbox.client.config.HttpClientConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.elasticsearch.jest.HttpClientConfigBuilderCustomizer;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 定制Jest底层的httpclient的config
+ * 部分配置可以通过配置文件的spring.elasticsearch.jes.xxx配置，如连接URL、connection timeout、read timeout
+ * 部分需要通过实现HttpClientConfigBuilderCustomizer定制
+ * 具体见：JestAutoConfiguration、JestClientFactory#getObjetc()
+ */
+@Component
+public class JestHttpClientConfigBuilderCustomizer implements HttpClientConfigBuilderCustomizer {
+
+    //最大连接数，默认值：30
+    @Value("${spring.elasticsearch.jest.maxTotalConnection:30}")
+    private int maxTotalConnection;
+
+    //默认每个路由的最大连接数，默认值：10
+    @Value("${spring.elasticsearch.jest.defaultMaxTotalConnectionPerRoute:10}")
+    private int defaultMaxTotalConnectionPerRoute;
+
+    //连接空闲多少秒会被认定为空闲连接并关闭，默认值：0，不开启空闲连接检测
+    @Value("${spring.elasticsearch.jest.maxConnectionIdleTimeInSecond:0}")
+    private long maxConnectionIdleTimeInSecond;
+
+    @Override
+    public void customize(HttpClientConfig.Builder builder) {
+        builder.maxTotalConnection(maxTotalConnection);  //最大连接数
+        builder.defaultMaxTotalConnectionPerRoute(defaultMaxTotalConnectionPerRoute);   //每个路由的最大连接数
+        builder.maxConnectionIdleTime(maxConnectionIdleTimeInSecond, TimeUnit.SECONDS); //连接空闲时间
+    }
+
+}
